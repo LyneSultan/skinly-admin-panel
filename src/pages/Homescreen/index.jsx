23b5f3c6d2fc6d5cea8a1cae560ead769bef377e
LyneSultan <lynesultane@gmail.com>
2025-01-05 21:30:19 +0200

    updated card in homescreen

--- src/pages/Homescreen/index.jsx
+++ src/pages/Homescreen/index.jsx
@@ -2,18 +2,20 @@ import './../../style/base.css';
 import './style.css';
 const HomeScreen = () => {
   return (
-    <div className='flex space-between'>
-      <div className="card">
-        <div className="card-content">
-          <span className="card-number">32</span>
-          <span className="card-text">New User This Week</span>
-          <div className="card-icon">
-
+    <div >
+      <div className="card flex justify-center align-center">
+        <div className="card-content ">
+          <div className='flex space-around'>
+            <div className="card-number">32</div>
+            <img src="/images/user.png" />
           </div>
+
+          <div className="card-text">Number of Skinly Users</div>
+
         </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 export default HomeScreen;
