ef85a6ecfa9ec903bc369f0fa9ef6d0d74158d59
LyneSultan <lynesultane@gmail.com>
2025-01-05 21:14:49 +0200

    added card content to home screen

--- src/pages/Homescreen/index.jsx
+++ src/pages/Homescreen/index.jsx
@@ -1,7 +1,17 @@
+import './../../style/base.css';
+import './style.css';
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className='flex space-between'>
+      <div className="card">
+        <div className="card-content">
+          <span className="card-number">32</span>
+          <span className="card-text">New User This Week</span>
+          <div className="card-icon">
+
+          </div>
+        </div>
+      </div>
 
     </div>
   )
