b55773cc5e2776f64ae0e812d8b0f15250bb2358
LyneSultan <lynesultane@gmail.com>
2025-01-13 01:55:26 +0200

    added layout to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomeScreen from './pages/Homescreen';
-import routes from './routes/routes';
+import Layout from './layout';
+import HomeScreen from './pages/Homescreen/index.jsx';
+import routes from './routes/routes.js';
 
 const App = () => {
   return (
     <>
+          <Layout>
+
       <Routes>
         <Route path={routes.login} element={<div>login</div>} />
         <Route path={routes.home} element={<HomeScreen />} />
-
-      </Routes>
+        </Routes>
+        </Layout>
     </>
   );
 };
