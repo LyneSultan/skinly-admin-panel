6562c12060716ddd763c7b85d16d043a460b2fda
LyneSultan <lynesultane@gmail.com>
2025-01-12 10:28:31 +0200

    removed home logic

--- src/pages/Homescreen/index.jsx
+++ src/pages/Homescreen/index.jsx
@@ -1,83 +1,13 @@
 import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "../../components/base/Button";
 import DashboardCard from "../../components/InfoCard";
 import './../../style/base.css';
 import './style.css';
+import useHomeLogic from "./useHomeLogic";
 
 const HomeScreen = () => {
-  const apiUrl = process.env.REACT_APP_SERVER_API;
-  const [users, setUsers] = useState([]);
-  const [companies, setCompanies] = useState([]);
-  const [view, setView] = useState("users");
-  const getUsers = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/users/");
-      setUsers(response.data.users);
-    } catch (error) {
-      console.error("Error fetching users:", error);
-    }
-  };
-
-  const getCompanies = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/company/");
-      setCompanies(response.data);
-    } catch (error) {
-      console.error("Error fetching companies:", error);
-    }
-  };
-
-  useEffect(() => {
-    getUsers();
-    getCompanies();
-  }, []);
-  const ban = async (userId) => {
-    try {
-      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imx5bmUiLCJzdWIiOiI2NzZmMzE2MjE1Y2FiMjUyMGM1NjI4MGIiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzY2MzI5NDQsImV4cCI6MTczNjYzNjU0NH0.EWJS3pmpEOtK8w72m84LdxOTyemeOOJCqpXuPguE_dA"; // Static token
-      const response = await axios.get(`http://localhost:3000/users/ban/${userId}`, {
-        headers: {
-          Authorization: token,
-        },
-      });
-      console.log(response.data);
-      const user = response.data;
-
-      setUsers((prevUsers) =>
-        prevUsers.map((u) =>
-          u._id === user._id ? { ...u, ban: user.ban } : u
-        )
-      );
-
-    } catch (error) {
-      console.error("Error banning user:", error);
-    }
-  };
-
-  const unban = async (userId) => {
-    try {
-      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imx5bmUiLCJzdWIiOiI2NzZmMzE2MjE1Y2FiMjUyMGM1NjI4MGIiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzY2MzI5NDQsImV4cCI6MTczNjYzNjU0NH0.EWJS3pmpEOtK8w72m84LdxOTyemeOOJCqpXuPguE_dA"; // Static token
-      const response = await axios.get(`http://localhost:3000/users/unban/${userId}`, {
-        headers: {
-          Authorization: token,
-        },
-      });
-
-      setUsers((prevUsers) =>
-        prevUsers.map((user) =>
-          user._id === userId ? { ...user, ban: false } : user
-        )
-      );
-    } catch (error) {
-      console.error("Error unbanning user:", error);
-    }
-  };
-
-  const handleViewChange = (viewType) => {
-    setView(viewType);
-  };
-
+  const {view,handleViewChange,users,companies,ban,unban, } = useHomeLogic();
   return (
     <div>
       <nav class="navbar">
@@ -101,7 +31,6 @@ const HomeScreen = () => {
 
       </div>
       <div className="flex space-around">
-        <Button title={"hello"} />
         <button onClick={() => handleViewChange("users")} class="view-toggle-btn">Users</button>
         <button onClick={() => handleViewChange("companies")} class="view-toggle-btn">Companies</button>
       </div>
