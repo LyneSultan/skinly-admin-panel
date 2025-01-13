897ecd50dcf5a698d68bc5df24f2a0ae916c41a8
LyneSultan <lynesultane@gmail.com>
2025-01-13 14:33:50 +0200

    added input component

---
+++ src/components/base/Input/index.jsx
@@ -0,0 +1,28 @@
+import { TextField } from '@mui/material';
+
+const Input = ({ id, label, type, value, onChange, ...props }) => {
+  return (
+    <TextField
+      margin="normal"
+      required
+      fullWidth
+      id={id}
+      label={label}
+      type={type}
+      value={value}
+      onChange={onChange}
+      sx={{
+        "& .MuiInputLabel-root": { color: "#D16F9A" },
+        "& .MuiOutlinedInput-root": {
+          "& > fieldset": { borderColor: "#D16F9A" },
+          "&:hover fieldset": { borderColor: "#D16F9A" },
+          "&.Mui-focused fieldset": { borderColor: "#D16F9A" },
+        },
+        "& .MuiInputBase-input": { color: "#D16F9A" },
+      }}
+      {...props}
+    />
+  );
+};
+
+export default Input;
