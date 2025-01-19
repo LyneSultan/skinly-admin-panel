5e3b9be9540c2352a0651416939d78f225e1a089
LyneSultan <lynesultane@gmail.com>
2025-01-19 22:35:56 +0200

    updated input component

--- src/components/base/Input/index.jsx
+++ src/components/base/Input/index.jsx
@@ -3,16 +3,12 @@ import { TextField } from '@mui/material';
 const Input = ({ id, label, type, value, onChange, ...props }) => {
   return (
     <TextField
-      margin="normal"
-      required
-      fullWidth
-      id={id}
-      label={label}
-      type={type}
-      value={value}
-      onChange={onChange}
+      margin="normal" required fullWidth
+      id={id} label={label} type={type}
+      value={value} onChange={onChange}
       sx={{
         "& .MuiInputLabel-root": { color: "#D16F9A" },
+        "& .MuiInputLabel-root.Mui-focused": { color: "#D16F9A" },
         "& .MuiOutlinedInput-root": {
           "& > fieldset": { borderColor: "#D16F9A" },
           "&:hover fieldset": { borderColor: "#D16F9A" },
