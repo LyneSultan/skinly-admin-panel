7341dff20283d295a62374bbb7d45f7be2d498f5
LyneSultan <lynesultane@gmail.com>
2025-01-19 22:36:44 +0200

    updated app file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './layout';
+import AddCompany from './pages/CreateCompany/index.jsx';
 import HomeScreen from './pages/Homescreen/index.jsx';
+import Login from './pages/Login/index.jsx';
 import routes from './routes/routes.js';
 
 const App = () => {
   return (
-    <>
-          <Layout>
+    <Routes>
+      <Route path={routes.login} element={<Login />} />
 
-      <Routes>
-        <Route path={routes.login} element={<div>login</div>} />
-        <Route path={routes.home} element={<HomeScreen />} />
-        </Routes>
-        </Layout>
-    </>
+      <Route path="/*"
+        element={
+          <Layout>
+            <Routes>
+              <Route path={routes.home} element={<HomeScreen />} />
+              <Route path={"add-company"} element={<AddCompany />} />
+
+            </Routes>
+          </Layout>
+        }
+      />
+    </Routes>
   );
 };
 
 export default App;
-
