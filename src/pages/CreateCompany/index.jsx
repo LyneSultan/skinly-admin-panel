86e5f44fb9c1ee9edf483811b6fdcf55fac48423
LyneSultan <lynesultane@gmail.com>
2025-01-19 22:34:51 +0200

    updated add company page

--- src/pages/CreateCompany/index.jsx
+++ src/pages/CreateCompany/index.jsx
@@ -1,104 +1,58 @@
 import { Box, Button, InputLabel, Paper, Typography } from '@mui/material';
-import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Input from '../../components/base/Input';
+import './style.css';
+import useCompanyLogic from './useCompanyLogic';
 
 const AddCompany = () => {
-  const [companyName, setCompanyName] = useState('');
-  const [companyEmail, setCompanyEmail] = useState('');
-  const [logo, setLogo] = useState(null);
-  const [file, setFile] = useState(null);
+  const { companyName, setCompanyName, companyEmail, setCompanyEmail, handleFileUpload, file, response, handleSubmit } = useCompanyLogic();
 
-  const handleLogoUpload = (e) => {
-    setLogo(e.target.files[0]);
-  };
-
-  const handleFileUpload = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const handleSubmit = async() => {
-    console.log('Company Name:', companyName);
-    console.log('Logo:', logo);
-    console.log('File:', file);
-    try {
-      const formData = new FormData();
-
-      formData.append('company_logo', "logo");
-      formData.append('scraping_file', "file");
-      formData.append('file', file);
-      formData.append('name', companyName);
-      formData.append('email', companyEmail);
-
-      const response = await axios.post("http://192.168.10.128:3000/company/", formData, {
-        headers: {
-          "Authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEiLCJzdWIiOiI2NzcxYzAzNDZjYTUyMTIxNzcwYTk3NjciLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzY3NjYzMDIsImV4cCI6MTczNjc2OTkwMn0.Gnmkb4GiBpa3LUhnEJNbpqupyUFMbXQHJC5iLJvZ8GQ",
-          'Content-Type': 'multipart/form-data',
-        },
+  useEffect(() => {
+    if (response) {
+      toast.success("Company Created!", {
+        autoClose: 3000,
       });
-    } catch (error) {
-      console.log(error);
     }
-  };
-
+  }, [companyName]);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '100vh',
-        backgroundColor: '#f5f5f5',
-        padding: 4,
-      }}
-    >
-      <Paper
-        sx={{
-          padding: 4,
-          width: 400,
-          textAlign: 'center',
-          backgroundColor: '#ffffff',
-          boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-        }}
-      >
+    <Box className="container">
+      <Paper className="paper">
+
         <Typography variant="h5" gutterBottom> Add Company</Typography>
 
-        <Box sx={{ marginBottom: 3 }}>
+        <Box className="input-box">
           <Input label="Company Name" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
         </Box>
 
-        <Box sx={{ marginBottom: 3 }}>
-          <Input label="Company Name"  value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+        <Box className="input-box">
+          <Input label="Company Email" value={companyEmail} onChange={(e) => setCompanyEmail(e.target.value)} />
         </Box>
 
-        <Box sx={{ marginBottom: 3 }}>
-          <InputLabel>Upload Company Logo</InputLabel>
-          <Button variant="outlined" component="label" fullWidth  sx={{ marginTop: 1 }}  >
-            Choose File
-            <input   type="file"  accept="image/*" hidden onChange={handleLogoUpload}  />
-          </Button>
 
-          {logo && (
-            <Typography variant="body2" color="text.secondary" mt={1}> Selected File: {logo.name} </Typography>
-          )}
-        </Box>
+        <ToastContainer />
 
-        <Box sx={{ marginBottom: 3 }}>
+
+        <Box className="upload-box">
           <InputLabel>Upload Supporting File</InputLabel>
-          <Button variant="outlined"component="label" fullWidth sx={{ marginTop: 1 }}>
+          <Button variant="outlined" component="label" fullWidth className="button">
+
             Choose File
-            <input type="file" hidden onChange={handleFileUpload}/>
+            <input type="file" hidden onChange={handleFileUpload} />
           </Button>
           {file && (
             <Typography variant="body2" color="text.secondary" mt={1}>Selected File: {file.name}</Typography>
           )}
         </Box>
+        {response && (
+          <Typography variant="body2" color="text.secondary" mt={1}> {response}</Typography>
+        )}
 
-        <Button variant="contained" color="primary" fullWidth  onClick={handleSubmit}> Submit </Button>
+        <Button variant="contained" fullWidth sx={{ backgroundColor: "#D16F9A" }} onClick={handleSubmit}> Submit </Button>
       </Paper>
-    </Box>
+    </Box >
   );
 };
 
